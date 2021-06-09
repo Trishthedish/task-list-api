7d6f0f46718f99ac9b16a5248aab65b54bded942
trish <pmgriego@gmail.com>
2021-06-09 15:23:48 -0700

    add endpoints for updating, deleting, and getting a single task

--- app/routes.py
+++ app/routes.py
@@ -29,3 +29,30 @@ def handle_tasks():
                 "completed_at": task.completed_at
             })
         return jsonify(tasks_response)
+
+@tasks_bp.route("/<task_id>", methods=["GET","PUT","DELETE"])
+def handle_task(task_id):
+    task = Task.query.get(task_id)
+    if task is None:
+        return make_response("", 404)
+
+    if request.method == "GET":
+        return {
+            "task_id": task.task_id,
+            "title": task.title,
+            "description": task.description,
+            "completed_at": task.completed_at
+        }
+
+    elif request.method == "PUT":
+        form_data = request.get_json()
+        task.title = form_data["title"]
+        task.description = form_data["description"]
+        task.completed_at = form_data["completed_at"]
+        db.session.commit()
+        return make_response(f"Task #{task.task_id} successfully updated")
+
+    elif request.method == "DELETE":
+        db.session.delete(task)
+        db.session.commit()
+        return make_response(f"Task #{task.task_id} successfully deleted.")
