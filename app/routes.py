a2cf3fc1cdbc5f9b8736e758a579bf97084fe410
trish <pmgriego@gmail.com>
2021-06-09 15:14:54 -0700

    initial set up and working get,post for a single task

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,36 @@
-from flask import Blueprint
+from flask.json import jsonify
+from app import db
+from app.models.task import Task
+from flask import request, Blueprint, make_response, jsonify
 
+tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+@tasks_bp.route("", methods=["GET","POST"])
+def handle_tasks():
+    if request.method == "POST":
+        request_body = request.get_json()
+        new_task = Task(
+            title=request_body["title"],
+            description=request_body["description"],
+            completed_at=request_body["completed_at"]
+        )
+        db.session.add(new_task)
+        db.session.commit()
+        return jsonify(f"Task {new_task.title} successfully created"), 201
+
+    elif request.method == "GET":
+        title_query = request.args.get("title")
+        if title_query:
+            tasks = Task.query.filter_by(title=title_query)
+        else:
+            tasks = Task.query.all()
+
+        tasks_response = []
+        for task in tasks:
+            tasks_response.append({
+                "task_id": task.task_id,
+                "title": task.title,
+                "description": task.description,
+                "completed_at": task.completed_at
+            })
+        return jsonify(tasks_response)
