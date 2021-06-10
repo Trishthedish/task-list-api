4335b1d9f97be68b4c8ea4a4d667013a3733b235
trish <pmgriego@gmail.com>
2021-06-09 23:31:36 -0700

    add goal model, and post goal endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -30,7 +30,9 @@ def create_app(test_config=None):
     migrate.init_app(app, db)
 
     from .routes import tasks_bp
+    from .routes import goals_bp
     # Register Blueprints here
     app.register_blueprint(tasks_bp)
+    app.register_blueprint(goals_bp)
 
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask.json import jsonify
 from app import db
 from app.models.task import Task
+from app.models.goal import Goal
 from flask import request, Blueprint, make_response, jsonify
 from datetime import date, datetime
 from dotenv import load_dotenv
@@ -9,6 +10,7 @@ import os
 
 load_dotenv()
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+goals_bp = Blueprint('goals', __name__, url_prefix="/goals")
 
 @tasks_bp.route("", methods=["GET","POST"])
 def handle_tasks():
@@ -158,3 +160,19 @@ def mark_incomplete(task_id):
             "is_complete": False
         }
     }, 200
+
+@goals_bp.route("", methods=["POST"])
+def handle_goals():
+    if request.method == "POST":
+        request_body = request.get_json()
+        new_goal = Goal(title=request_body["title"])
+        db.session.add(new_goal)
+        db.session.commit()
+
+        return make_response({
+            "goal": {
+                "id": new_goal.goal_id,
+                "title": new_goal.title
+
+            }
+        }, 201)
