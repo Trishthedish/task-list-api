c0051a53c2b87cda00ac16b317bef650551aa2c7
trish <pmgriego@gmail.com>
2021-06-14 13:19:11 -0700

    create one to many relationship in models

--- app/models/goal.py
+++ app/models/goal.py
@@ -5,3 +5,4 @@ from app import db
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
+    tasks = db.relationship("Task", backref="goal", lazy=True)
